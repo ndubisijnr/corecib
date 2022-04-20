49f388b0c0dcd14584ac3893cff1a649fa296c1e
Gingercapo <christianchinedu19@gmail.com>
2022-04-20 01:01:37 +0100

    Pushing Organisation and Dispute Update

--- src/service/DisputeService.js
+++ src/service/DisputeService.js
@@ -1,12 +1,44 @@
-import { apiClient } from "./BaseService";
+import { apiClient} from "./BaseService";
+
+import axios from "axios";
+import { qgBaseUrl } from "../app.config";
+import store from "../store/store";
+
+const apiClient = axios.create({
+    baseURL: qgBaseUrl,
+    withCredentials: false,
+    headers: {
+        //Accept: "application/json",
+        //"Access-Control-Allow-Origin": "*",
+        "Content-Type": "application/json",
+    }
+});
+
+const apiClient2 = axios.create({
+    baseURL: qgBaseUrl,
+    withCredentials: false,
+    headers: {
+        //Accept: "application/json",
+        "Content-Type": "application/json",
+        //"Access-Control-Allow-Origin": "*",
+    }
+});
 
 export default {
-    callReadDisputeApi(payload){
-      return apiClient.post('dispute/read', payload)
+    callCreateDisputeApi(payload){
+      return apiClient.post('/dispute/create', payload)
   
     },
-    callCreateDisputeApi(payload){
-      return apiClient.post('dispute/create', payload)
+    callReadDisputeApi(payload){
+      return apiClient.post('/dispute/read', payload)
+
+    },
+    callReadSingleDisputeApi(payload){
+      return apiClient.post('/dispute/read-by-id', payload)
+
+    },
+    callUpdateDisputeApi(payload){
+      return apiClient.post('/dispute/update', payload)
   
     }
 
--- src/service/OrganizationService.js
+++ src/service/OrganizationService.js
@@ -1,9 +1,51 @@
 import { apiClient} from "./BaseService";
 
+import axios from "axios";
+import { qgBaseUrl } from "../app.config";
+import store from "../store/store";
+
+const apiClient = axios.create({
+  baseURL: qgBaseUrl,
+  withCredentials: false,
+  headers: {
+    //Accept: "application/json",
+    //"Access-Control-Allow-Origin": "*",
+    "Content-Type": "application/json",
+  }
+});
+
+const apiClient2 = axios.create({
+  baseURL: qgBaseUrl,
+  withCredentials: false,
+  headers: {
+    //Accept: "application/json",
+    "Content-Type": "application/json",
+    //"Access-Control-Allow-Origin": "*",
+  }
+});
+// https://cors-anywhere.herokuapp.com
 
 
 export default {
-  callReadOrganisationByIdApi(payload){
-    return apiClient.post('/organisation/read-by-id', payload)
-  }
-}
+    callUpdateOrganistionByIdApi(payload) {
+      // console.log("Service Payload ==>>", payload)
+      return apiClient.post("/organisation/update", payload);
+    },
+    callTransactionEnquiryApi(payload) {
+      // console.log("Service Payload ==>>", payload)
+      return apiClient.post("/organisation/transaction-enquiry", payload);
+    },
+    callBalanceEnquiryApi(payload) {
+      // console.log("Service Payload ==>>", payload)
+      return apiClient.post("/organisation/balance-enquiry", payload);
+    },
+    callReadOrganisationByIdApi(payload) {
+      // console.log("Service Payload ==>>", payload)
+      return apiClient.post("/organisation/read-by-id", payload);
+    },
+    callReadUsersByOrganisationIdApi(payload) {
+      // console.log("Service Payload ==>>", payload)
+      return apiClient.post("/organisation/read-users-by-organisation-id", payload);
+    }
+};
+
